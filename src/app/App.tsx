b7dd3a7ad5bff70939de905e385c1f320782a9f5
Andrew Yanushkevich <andy.yanushkevich@gmail.com>
2026-02-01 11:07:45 +0300

    feat(app): add redux config provider and antd provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./App.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes.tsx";
+import { ConfigProvider } from "antd";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#1677ff",
+          borderRadius: 12,
+        },
+      }}
+    >
+      <Provider store={store}>
+        <RouterProvider router={router}></RouterProvider>
+      </Provider>
+    </ConfigProvider>
   </StrictMode>
 );
