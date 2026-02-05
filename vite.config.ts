c915175095fed0294779fbb4eb0b2a7a754c5f2a
Andrew Yanushkevich <andy.yanushkevich@gmail.com>
2026-02-05 17:30:31 +0300

    chore: add vite ts checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: { buildMode: true },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
